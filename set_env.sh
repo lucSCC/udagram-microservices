f28b4721cf5e89ae918b6005faf05bfb0f3a6451
Lucas Gompou <gompou16@univmetiers.ci>
2021-11-13 11:23:51 -0500

    initial commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=Guva0705_
+export POSTGRES_HOST=udagram-db-dev.csgfccllztbl.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udagram-106071795573-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
